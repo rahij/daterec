9f3567051409d4080293c7ab8a7dbd4234a8b135
rahij <rahij.ramsharan@gmail.com>
2012-11-08 17:14:47 +0800

    correct date triggered; more date formats

--- content/overlay.js
+++ content/overlay.js
@@ -18,6 +18,17 @@ var months={
   "October": 10,
   "November": 11,
   "December": 12, 
+  "Jan" : 1, 
+  "Feb" : 2,
+  "Mar" : 3,
+  "Apr": 4,
+  "June": 6,
+  "Jul": 7,
+  "Aug":8,
+  "Sept" : 9,
+  "Oct": 10,
+  "Nov": 11,
+  "Dec": 12, 
   "01": 1,
   "1": 1,
   "02": 2,
@@ -42,10 +53,6 @@ var months={
 };
 
 var DateRec = {
-
-  test: function(){
-    alert("::");
-  },
   do: function () {
     // First we get msgHdr, a nsIMsgDbHdr
     
@@ -69,23 +76,58 @@ var DateRec = {
     MsgHdrToMimeMessage(msgHdr, null, function (aMsgHdr, aMimeMsg) {
       var message_body=(aMimeMsg.coerceBodyToPlaintext());
       
+      var found = new Array();
       var regex=new Array(
-        /(\d+)\/(\d+)\/(\d+)/g,
-        /(\d+)-(\S+)-(\d+)/g,
-        /(\d+) (\S+) (\d+)/g,
-        /(\d+)(\S+) (\S+) (\d+)/g
+        /(\d+)\/(\d+)\/(\d+)/g,  // 0. 3/5/1994 , 03/05/1994, 3/05/1994/ 03/5/1994   
+        /(\d+)-(\S+)-(\d+)/g,    // 1. Same as above; but - instead of /
+        /(\d+) (\S+) (\d+)/g,     // 2. 3 May 1994, 03 May 1994
+        /(\d+)(\S+) (\S+) (\d+)/g,  // 3. 3rd May 1994
+        /(\d+) (\S+), (\d+)/g,       // 4. 3 May, 1994
+        /(\S+) (\d+)(\S+) (\d+)/g,   // 5. May 3rd 1994
+        /(\S+) (\d+), (\d+)/g,           // 6. May 3, 1994
+        /(\S+) (\d+) (\d+)/g           // 7. May 3 1994
+        
       );
-      
+
+      var detectedDescription = "<div id = 'detectedDescription' style='margin-bottom: 15px;'>Dates Found</div>";
+      $(document.getElementById("messagepane").contentDocument.body).append(detectedDescription);
+
       var num_dates=0,match;
       for(var i=0;i<regex.length;++i){
         
         while(match=regex[i].exec(message_body)){  
           
-          var day_index,month_index=2,year_index;
+          if(found.indexOf(match[0]) != -1)
+            continue;
+          else
+            found.push(match[0]);
+
+          var day_index=1,month_index=2,year_index=3;
+          
           if(i==3)
           {
             month_index=3;
+            year_index = 4;
           }
+
+          if(i==4)
+          {
+            year_index = 4;
+          }
+
+          if(i==5 || i==6)
+          {
+            month_index = 1;
+            day_index = 2;
+            year_index = 4;
+          }
+
+          if(i == 7)
+          {
+            day_index = 2;
+            month_index = 1;
+          }
+          
           if((match==null) || !(match[month_index] in months))
             break;
           ++num_dates;
@@ -93,57 +135,28 @@ var DateRec = {
           var v = document.getElementById("messagepane").contentDocument.body;
         
           var bodyTextElement = document.createElement('span');
-          bodyTextElement.setAttribute('day','3');
+          bodyTextElement.setAttribute('day',match[day_index]);
+          bodyTextElement.setAttribute('month',match[month_index]);
+          bodyTextElement.setAttribute('year',match[year_index]);
           bodyTextElement.setAttribute('style','display:block;text-decoration: underline; color: blue;cursor:pointer;');
           var t=document.createTextNode(match[0]);
           bodyTextElement.appendChild(t);    
 
           bodyTextElement.addEventListener("click",function(){
             var day = this.getAttribute("day");
-            new_event_with_dates(day,9,2012);
+            var month = this.getAttribute("month");
+            var year = this.getAttribute("year");
+            new_event_with_dates(day,month,year);
           });
-          
-          v.appendChild(bodyTextElement);
-          continue;
-          
-          
-          if (confirm("Add event on " + match[0]+ "?")) { 
-            if(i!=3)
-              new_event(match);
-            else
-              new_event(match,1,3,4);  
-          }
+
+          $(v).append(bodyTextElement);
         }
       }
         
       top();
-
     });
   }
 };
-
-function new_event(match,day_index=1,month_index=2,year_index=3){
-  var newItem = createEvent();
-  var aMsgHdr=gFolderDisplay.selectedMessage;
-  var aItem=newItem;
-
-  aItem.calendar = getSelectedCalendar();
-  aItem.title = aMsgHdr.mime2DecodedSubject;
-  cal.setDefaultStartEndHour(aItem);
-  
-  aItem.startDate.second=0;
-  aItem.startDate.minute=0;
-  aItem.startDate.hour=16;
-  aItem.startDate.day=match[day_index];
-
-  aItem.startDate.month=months[match[month_index]]-1;
-  
-  aItem.startDate.year=match[year_index];
-
-  aItem.endDate = aItem.startDate.clone();
-  aItem.endDate.hour += 1;
-  createEventWithDialog(null, null, null, null, aItem);
-}
 
 function new_event_with_dates(day,month,year){
   var newItem = createEvent();
@@ -159,10 +172,11 @@ function new_event_with_dates(day,month,year){
   aItem.startDate.hour=16;
   
   aItem.startDate.day=day;
-  aItem.startDate.month=month;
+  aItem.startDate.month=months[month]-1;
   aItem.startDate.year=year;
 
   aItem.endDate = aItem.startDate.clone();
   aItem.endDate.hour += 1;
+
   createEventWithDialog(null, null, null, null, aItem); 
 }
